1ebff3928b8c07efeb99726fcbbf3a53682a6434
MOSUMA <65025326+TDVAppLab@users.noreply.github.com>
2022-08-09 20:16:17 +0900

    Make an object's size configurable by instance
    /issues/33

--- TDIC/client-app/src/features/Articles/common/modelscreen/LoadModel.tsx
+++ TDIC/client-app/src/features/Articles/common/modelscreen/LoadModel.tsx
@@ -13,26 +13,28 @@ import React from 'react';
 interface PartProps {
     id_part: number;
     pos:Vector3;
+    scale:number;
 }
 
 
 
-const LoadModel  = ({id_part, pos}: PartProps) => {
+const LoadModel  = ({id_part, pos, scale}: PartProps) => {
     return (
         <React.Suspense fallback={null}>
-            <LoadModelSub id_part={id_part} pos={pos} />
+            <LoadModelSub id_part={id_part} pos={pos} scale={scale} />
         </React.Suspense>
     )
 }  
 
 
 
-const LoadModelSub  = ({id_part, pos}: PartProps) => {
+const LoadModelSub  = ({id_part, pos, scale}: PartProps) => {
 
   
     const str_url_partapi = process.env.REACT_APP_API_URL + `/modelfiles/file/${id_part}`;
     const gltf = useLoader(GLTFLoader, str_url_partapi);
     gltf.scene.position.set(pos.x,pos.y,pos.z);
+    gltf.scene.scale.set(scale,scale,scale)
     //console.log(id_part);
   
     return (
