5cdcd4f2b8f92442cf3f56979bc6024dc3443bef
MOSUMA <65025326+TDVAppLab@users.noreply.github.com>
2022-08-12 15:15:34 +0900

    [ModelfileDashboard][FR]Allow referenced records to be excluded
    /issues/61

--- TDIC/client-app/src/app/api/agent.ts
+++ TDIC/client-app/src/app/api/agent.ts
@@ -77,7 +77,7 @@ const requests = {
 }
 
 const Modelfiles = {
-    list: () => requests.get<Modelfile[]>('/modelfiles/index'),
+    list: (is_exclude_used:boolean) => requests.get<Modelfile[]>(`/modelfiles/index/is_exclude_used=${is_exclude_used}`),
     details:(id:number) => requests.get<Modelfile>(`/modelfiles/details/${id}`),
     update: (modelfile: Modelfile) => axios.post<void>(`/modelfiles/update`, modelfile),
     fileupload: (formData:FormData) => axios.post('/modelfiles/uploadfile',formData),
--- TDIC/client-app/src/features/Modelfiles/dashboard/ModelfileDashboard.tsx
+++ TDIC/client-app/src/features/Modelfiles/dashboard/ModelfileDashboard.tsx
@@ -1,23 +1,37 @@
 import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
 import { useStore } from '../../../app/stores/store';
 import ModelfileList from './ModelfileList';
 
-export default observer(function ModelfileDashboard() {        
+export default observer(function ModelfileDashboard() {
+
+    const [isExcludeUsed, setIsExcludeUsed] = useState(false);
+
+
     const {modelfileStore} = useStore();
     const {loadModelfiles, ModelfileRegistry} = modelfileStore;
   
     useEffect(() => {
-        loadModelfiles();
+        loadModelfiles(isExcludeUsed);
     },[])
+  
+    useEffect(() => {
+        loadModelfiles(isExcludeUsed);
+    },[isExcludeUsed])
 
 
     useEffect(() => {
-        if(ModelfileRegistry.size <= 1) loadModelfiles();
+        if(ModelfileRegistry.size <= 1) loadModelfiles(isExcludeUsed);
     },[ModelfileRegistry.size, loadModelfiles])
+
+
+    
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsExcludeUsed(event.target.checked);
+    }
   
   
     if(modelfileStore.loading) return <LoadingComponent content='Loading modelfiles...' />
@@ -29,8 +43,13 @@ export default observer(function ModelfileDashboard() {
             <Link to={`/modelfilecreate`}>
                 <h3 >Create</h3>
             </Link>
-            
             <hr />
+            
+            <div className="App">
+                <input type="checkbox" defaultChecked={isExcludeUsed} onChange={handleChange}/>
+                <label>Exclude Used Models</label>
+            </div>
+            
             <ModelfileList />
         </Container>
 
