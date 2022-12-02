153891c1d6d7f12299feecfc26caeefffcd2b6eb
MOSUMA <65025326+TDVAppLab@users.noreply.github.com>
2022-12-02 23:03:24 +0900

    Create a quaternion type for the model.(delete null)
    /issues/217

---
+++ TDIC/server-app/Migrations/20221202133820_addQuotforInstance2.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace serverapp.Migrations
+{
+    public partial class addQuotforInstance2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "quaternion_z",
+                table: "t_instance_object",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "quaternion_y",
+                table: "t_instance_object",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "quaternion_x",
+                table: "t_instance_object",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "quaternion_w",
+                table: "t_instance_object",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "quaternion_z",
+                table: "t_instance_object",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "quaternion_y",
+                table: "t_instance_object",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "quaternion_x",
+                table: "t_instance_object",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "quaternion_w",
+                table: "t_instance_object",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
